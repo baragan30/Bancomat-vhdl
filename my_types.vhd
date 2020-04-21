946242696127aa1dfef94e977fc2dcaf4d0c89bd
baragan30 <62956822+baragan30@users.noreply.github.com>
2020-04-21 15:39:55 +0300

    Hi1

--- my_types.vhd
+++ my_types.vhd
@@ -3,10 +3,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 package	my_types is   
-	subtype	digit is integer range 0 to 10;
+	subtype	digit is integer range 0 to 9;
 	type array4digits is array (3 downto 0) of digit;
 	subtype BCD is std_logic_vector (6 downto 0);
-	type array4BCD is array (3 downto 0) of BCD;
+	type array4BCD is array (3 downto 0) of BCD;  
+	subtype	number is integer range 0 to 9999;
 	
 	
 end package;
