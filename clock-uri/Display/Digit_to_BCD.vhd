dc2bf59303d00ac2f4dbf087e5b7b08ccb30116d
unknown <you@example.com>
2020-04-24 17:04:22 +0300

    MERGE

---
+++ clock-uri/Display/Digit_to_BCD.vhd
@@ -0,0 +1,31 @@
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.my_types.all;
+
+
+use IEEE.NUMERIC_STD.ALL;
+
+entity Digit_to_BCD is
+  Port ( 
+  sw:in digit;
+  afisor:out BCD);
+end Digit_to_BCD;
+
+architecture a_digit_to_BCD of Digit_to_BCD is
+begin	  --segements1(0)(0)
+     afisor<=
+     "0000001" when sw=0 else --0
+     "1001111" when sw=1 else --1
+     "0010010" when sw=2 else --2
+     "0000110" when sw=3 else --3
+     "1001100" when sw=4 else --4
+     "0100100" when sw=5 else --5
+     "0100000" when sw=6 else --6
+     "0001111" when sw=7 else --7
+     "0000000" when sw=8 else --8
+     "0000100" when sw=9 else --9
+     "0110000";               --E de la eroare
+
+end a_digit_to_BCD;
---
+++ clock-uri/Display/master_display.vhd
@@ -0,0 +1,65 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;  
+
+use work.my_types.all;
+
+entity Display_to_BCD is
+	port (
+	CLK :in std_logic;
+	intreg1:in number;
+	intreg2:in number;
+	afisor:out BCD;
+	segments:out std_logic_vector(7 downto 0)
+	);
+end Display_to_BCD;
+
+architecture Display_to_BCD of Display_to_BCD is 
+component Display_number is  
+	port (
+	numar:in number;
+	signals:out array4BCD
+	);
+end component;
+
+component Clock1khz is	
+		port (
+	clockin :in std_logic;
+	clockout :inout std_logic
+	);
+end component;
+
+signal BCDs :array8BCD; 
+signal BCDs1 :array4BCD;
+signal BCDs2 :array4BCD;
+signal CLK1khz : std_logic := '0';
+begin  
+	G1: Display_number port map(intreg1,BCDs1);	 
+	G3: Display_number port map(intreg2,BCDs2);
+	G2:Clock1khz port map (CLK,CLK1khz);
+	BCDs(0)<=BCDs1(0);
+	BCDs(1)<=BCDs1(1);
+	BCDs(2)<=BCDs1(2);
+	BCDs(3)<=BCDs1(3);
+	BCDs(4)<=BCDs2(0);
+	BCDs(5)<=BCDs2(1);
+	BCDs(6)<=BCDs2(2);
+	BCDs(7)<=BCDs2(3);
+	
+	process (CLK1khz)
+	variable numarator : integer range 0 to 8;
+	begin	
+		if (CLK1khz='1' and CLK1khz'event) then 
+			if(numarator=7)then 
+				numarator:=0;
+			else
+				numarator:=numarator+1;
+			end if;
+			afisor<=BCDs(numarator);
+			segments<= (others =>'1');
+			segments(numarator)<='0' ;
+				
+		end if;		
+		
+	end process;
+
+end Display_to_BCD;
