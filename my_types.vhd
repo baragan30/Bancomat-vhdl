9ba535fccc9e14dc508bdfa735a67ccf41d42a6f
unknown <you@example.com>
2020-04-24 18:28:50 +0300

    Display

--- my_types.vhd
+++ my_types.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.all;
 
 package	my_types is   
     type switch is array (3 downto 0) of std_logic;
-	subtype	digit is integer range 0 to 9;
+	subtype	digit is integer range 0 to 12;
 	type array4digits is array (3 downto 0) of digit;
 	subtype BCD is std_logic_vector (6 downto 0);
 	type array4BCD is array (3 downto 0) of BCD;
