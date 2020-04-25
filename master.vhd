51a7ee0f7585638ee8b2ba50787fc1107774eb16
baragan30 <62956822+baragan30@users.noreply.github.com>
2020-04-25 15:10:46 +0300

    master diplay

--- master.vhd
+++ master.vhd
@@ -14,36 +14,60 @@ entity master is
 end master;
 
 architecture master of master is 
+--------------------------------------clock-uri---------------------------------------
  component clock02sec is 
 	port (
 	clockin :in std_logic;
 	clockout :inout std_logic
 	);
+end component;	
+component Clock1khz is	
+		port (
+	clockin :in std_logic;
+	clockout :inout std_logic
+	);
 end component;
+
 component read_integer is
   Port (
   clk: in std_logic;
   sw: in switch;
   numar: out number);
 end component;	
+---------------------------------------entitati display---------------------------
 component number_to_digits is  
 	port (
 	numar:in number;
-	signals:out array4BCD
+	digits:out array4digits
 	);
-end component;
+end component;	
+entity master_display is
+	port (
+	CLK :in std_logic;
+	cifre1:in array4digits;
+	cifre2:in array4digits;
+	afisor:out BCD;
+	segments:out std_logic_vector(7 downto 0)
+	);
+end master_display;
 
-signal clock02s :std_logic;
+--------------------------------------------semnale--------------------------------------
+signal clk02s :std_logic;	
+signal clk1khz:std_logic;
+---------------Display
 signal afisor1:array4digits;
-signal afisor2:array4digits; 
+signal afisor2:array4digits;
+signal cifre1:array4digits;
+signal cifre2:array4digits; 
+signal numar:number:=0;
+signal numar1:number:=0;
+signal numar2:number:=0; 
+------------------RAM
 signal pin:number;
 signal cod:number;
 signal corect:std_logic;--daca pinul e corect
-signal numar:number:=0;
-signal numar1:number:=0;
-signal numar2:number:=0;
-signal cifre1:array4digits;
-signal cifre2:array4digits;
+
+
 begin 	   
 
 -------------------------------------------organigrama---------------------------------------
@@ -87,7 +111,8 @@ begin
 		
 	end process; 
 	
-	G1:clock02sec port map(clk,clock02s);
+	c1:clock02sec port map(clk,clock02s); 
+	c2:
 	G2:read_integer port map (clock02s,sw,numar);
 	--G3:number_to_digits port map(numar1,cifre1); 
 	--G4:number_to_digits port map(numar2,cifre2);
