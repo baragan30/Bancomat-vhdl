041abfe0bceffe495ff4f7be65b22c0421c3141c
baragan30 <62956822+baragan30@users.noreply.github.com>
2020-05-02 13:58:36 +0300

    iara

--- master.vhd
+++ master.vhd
@@ -80,70 +80,122 @@ signal pin:number;
 signal cod:number;
 signal sumin: number;
 signal sumout: number;
-signal codout: number;
+signal pinout: number;
 signal corect:std_logic;--daca pinul e corect
-signal semnalRAM:digit:=4;
+signal semnalRAM:digit:=4; 
 
+signal nou :std_logic;
 
-begin 	   
-	process(ok,exi,back)
-	variable stare :number :=0;
+begin 	
+	
+	process(ok,exi,back,clk)
+	variable stare :number :=4;
+	variable codcopy:number:=3;
 	begin 
-		case stare is 
+		cod<=codcopy;
+		case stare is
+----------------------------------------------------------Start---------------------------------------------------
 			when 0=>
-			pin<=1234;
-			semnalRAM<=0;
-			when 1=> 
-			pin<=2345 ;
-			semnalRAM<=0;
-			when 2=>
-			semnalRAM<=1;
-			pin<=2345;
+			numar2<=stare;
+			afisor2<=cifre2;
+			afisor1<=(0,0,0,0);
+			if(ok'event and ok ='1')then 
+				case sw is 
+					when "0001" =>stare:=1;
+					when "0000" =>stare:=2;	
+					when others => stare:=stare;
+				end case;
+			end if;	
+----------------------------------------------------------Admin-------------------------------------------------
+			when 1=>
+			numar2<=stare;
+			afisor2<=cifre2;
+			numar1<=numar;
+			afisor1<=cifre1;
+			codcopy:=0;	
+			pin<=numar;
+			if(ok'event and ok ='1' and corect='1')then 
+				stare:=3;
+			end if;
+			if(back'event and back ='1' )then 
+				stare:=0;
+			end if;	
+			if(exi'event and exi ='1')then 
+				stare:=0;
+			end if;	
+--------------------------------------------------------Selector Admin------------------------------------------------
 			when 3=>
-			pin<=1234;
-			semnalRAM<=0;
-			when 4=> 
-			semnalRAM<=1;
-			pin<=1234; 
+			numar2<=stare;
+			afisor2<=cifre2;
+			afisor1<=(0,0,0,0);
+			if(ok'event and ok ='1')then 
+				stare:=3;
+			end if;
+			if(back'event and back ='1' )then 
+				stare:=1;
+			end if;	
+			if(exi'event and exi ='1')then 
+				stare:=0;
+			end if;	
+----------------------------------------------------Client card-------------------------------------
+			when 2=>
+			numar2<=stare;
+			afisor2<=cifre2;
+			numar1<=numar;
+			afisor1<=cifre1;
+			codcopy:=numar;
+			if(ok'event and ok ='1' )then 
+				stare:=4;
+			end if;
+			if(back'event and back ='1' )then 
+				stare:=0;
+			end if;	
+			if(exi'event and exi ='1')then 
+				stare:=0;
+			end if;
+--------------------------------------------------------Client PIN------------------------------------------------
+			when 4=>
+			numar2<=stare;
+			afisor2<=cifre2;
+			numar1<=numar;
+			afisor1<=cifre1;
+			pin<=numar;
+			if(ok'event and ok ='1' and corect='1')then 
+				stare:=5;
+			end if;
+			if(back'event and back ='1' )then 
+				stare:=0;
+			end if;	
+			if(exi'event and exi ='1')then 
+				stare:=0;
+			end if;	
+-------------------------------------------------------------Selector client-----------------------------------------
 			when 5=>
-			semnalRAM<=0;
-			pin<=1234;
-			when 6=>
-			semnalRAM<=4;
-			pin<=2345  ; 
-			when 7=>
-			sumin<=100;
-			semnalRAM<=2; 
-			when 8=>
-			sumin<=100;
-			semnalRAM<=2;
-			when 9=>
-			semnalRAM<=0;
-			when 10=> 
-			sumin<=25;
-			semnalRAM<=3;
-			when 11=>
-			SEMNALRAM<=0;
-			when 12=> 
-			semnalRAM<=3;
-			sumin<=5;
+			numar2<=stare;
+			afisor2<=cifre2;
+			afisor1<=(0,0,0,0);
+			if(ok'event and ok ='1')then 
+				stare:=3;
+			end if;
+			if(back'event and back ='1' )then 
+				stare:=1;
+			end if;	
+			if(exi'event and exi ='1')then 
+				stare:=0;
+			end if;	
+			
+			
 			when others => 
 			stare:=stare; 
 		end case;
 	end process; 
-	cod<=0;
-	numar2<=sumout;
-	numar1<=codout;
-	afisor1<=cifre1;
-	afisor2<= cifre2;
-	led<=corect;
 
 	c1:clock02sec       port map(clk,clk02s); 
 	c2:Clock1khz        port map (clk,clk1khz);
 	G1:read_integer     port map (clk02s,sw,numar);
 	G2:number_to_digits port map(numar1,cifre1); 
 	G3:number_to_digits port map(numar2,cifre2); 
-	G4: Memorie_RAM     port map(clk1khz,cod,pin,sumin,sumout,codout,semnalRAM,corect);
+	G4: Memorie_RAM     port map(clk1khz,cod,pin,sumin,sumout,pinout,semnalRAM,corect);
 	
 	Af1:master_display port	map(clk1khz,afisor2,afisor1,afisor,segments);	
 
