7db29ade9e40fba74d2636ab691037b767d2abe8
unknown <you@example.com>
2020-05-12 18:03:20 +0300

    FML

--- Greedy.vhd
+++ Greedy.vhd
@@ -4,10 +4,10 @@ use work.my_types.all;
 
 entity Greedy is
   Port (
-  start: in std_logic;
-  pret: in number;
+  start: in std_logic:='0';
+  pret: in number:=800;
   clk:in std_logic;	
-  bancnote_initiale:in arraybancnota;
+  bancnote_initiale:in arraybancnota:=(20,20,20,20,20,20,20);
   bancnote_ramase: out arraybancnota;
   bancnote_extrase: out arraybancnota;
   corect:out std_logic;
@@ -17,69 +17,89 @@ end Greedy;
 
 architecture Behavioral of Greedy is
 signal semnal_valoare: number:=pret;
+signal pret_modif:number:=0;
+signal valoare:number;
+signal cant:arraybancnota;
+signal  f:arraybancnota;
+signal gata:std_logic:='0';
+signal inventiv:std_logic:='0';
 begin
     process(clk)
-    variable valoare:number;
-    variable cant:arraybancnota;
-    variable  f:arraybancnota;
-    variable pret_modif:number:=0;
     begin
+    inventiv<='0';
+    if(start='0')then
+    valoare<=pret;
+    cant<=bancnote_initiale;
+    end if;
+    if(clk='1' and clk'event) then 
+    if (gata='0')then
     if(start='1') then
-    cant:=bancnote_initiale;
-    valoare:=pret;
     
-    if not(pret_modif=0) then
-    valoare:=pret_modif;
-    end if;
-    
-    if (clk'event and clk='1')then
         if semnal_valoare=800 and bancnote_initiale(5)>0 then
-            valoare:=valoare-200;
-            cant(5):=cant(5)-1;
-            f(5):=f(5)+1;
+            valoare<=valoare-200;
+            cant(5)<=cant(5)-1;
+            f(5)<=f(5)+1;
         elsif semnal_valoare=600 and bancnote_initiale(5)>0 then
-            valoare:=valoare-200;
-            cant(5):=cant(5)-1;
-            f(5):=f(5)+1;
+            valoare<=valoare-200;
+            cant(5)<=cant(5)-1;
+            f(5)<=f(5)+1;
         elsif semnal_valoare>=500 and bancnote_initiale(6)>0 then
-            valoare:=valoare-500;
-            cant(6):=cant(6)-1;
-            f(6):=f(6)+1;
+            valoare<=valoare-500;
+            cant(6)<=cant(6)-1;
+            f(6)<=f(6)+1;
         elsif semnal_valoare>=200 and bancnote_initiale(5)>0 then
-            valoare:=valoare-200;
-            cant(5):=cant(5)-1;
-            f(5):=f(5)+1;
+            valoare<=valoare-200;
+            cant(5)<=cant(5)-1;
+            f(5)<=f(5)+1;
         elsif semnal_valoare>=100 and bancnote_initiale(4)>0 then
-            valoare:=valoare-100;
-            cant(4):=cant(4)-1;
-            f(4):=f(4)+1;
+            valoare<=valoare-100;
+            cant(4)<=cant(4)-1;
+            f(4)<=f(4)+1;
         elsif semnal_valoare>=50 and bancnote_initiale(3)>0 then
-            valoare:=valoare-50;
-            cant(3):=cant(3)-1;
-            f(3):=f(3)+1;
+            valoare<=valoare-50;
+            cant(3)<=cant(3)-1;
+            f(3)<=f(3)+1;
         elsif semnal_valoare>=10 and bancnote_initiale(2)>0 then
-            valoare:=valoare-10;
-            cant(2):=cant(2)-1;
-            f(2):=f(2)+1;
+            valoare<=valoare-10;
+            cant(2)<=cant(2)-1;
+            f(2)<=f(2)+1;
         elsif semnal_valoare>=5 and bancnote_initiale(1)>0 then
-            valoare:=valoare-5;
-            cant(1):=cant(1)-1;
-            f(1):=f(1)+1;
+            valoare<=valoare-5;
+            cant(1)<=cant(1)-1;
+            f(1)<=f(1)+1;
         elsif semnal_valoare>=1 and bancnote_initiale(0)>0 then
-            valoare:=valoare-1;
-            cant(0):=cant(0)-1;
-            f(0):=f(0)+1;
+            valoare<=valoare-1;
+            cant(0)<=cant(0)-1;
+            f(0)<=f(0)+1;
+        else
+            final<='1';
+            inventiv<='1';
         end if;
-    end if;
-    semnal_valoare<=valoare;
-    bancnote_extrase<=f;
-    bancnote_ramase<=cant;
-    pret_modif:=valoare;
-    if(valoare>0) then
+    if(valoare>0 or inventiv='1') then
     	corect<='0';
     else
         corect<='1';
+        final<='1';
+        gata<='1';
+
     end if;
     end if;
+    
+    end if;
+    end if;
+    end process;
+    
+    semnal_valoare<=valoare;
+    
+    process(clk)
+    begin
+    if(clk='1' and clk'event) then
+    
+    bancnote_extrase<=f;
+    bancnote_ramase<=cant;
+
+    
+    end if;
+    
     end process;
 end Behavioral;
