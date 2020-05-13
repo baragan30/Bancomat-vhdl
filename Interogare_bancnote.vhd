a20535cb44392f24f4dd508a5dddc405325f711d
baragan30 <62956822+baragan30@users.noreply.github.com>
2020-05-13 22:32:36 +0300

    new

--- Interogare_bancnote.vhd
+++ Interogare_bancnote.vhd
@@ -1,46 +1,35 @@
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.my_types.all;
 
 entity Interogare_bancnote is
      Port ( 
-            clk:in std_logic;
-            codout:out number;
+	 		clk:in std_logic;
+	 		reset:in std_logic;
+            pozitie_bancnota:out number;
             tip_bancnota:out number
           );
 end Interogare_bancnote;
 
-architecture Behavioral of Interogare_bancnote is
-signal numarator:number:=501;
-signal pozitie:number:=6;
-signal bancnota_curenta:number:=0;
-begin
-    process(clk)
-    begin
-        
-         
-        if(clk='1' and clk'event) then
-            if(numarator=0)then
-            numarator<=501;
-            else
-            numarator<=numarator-1;
-            end if;
-            if(numarator=1 or numarator=5 or numarator=10 or numarator=50 or 
-               numarator=100 or numarator=200 or numarator=500) then
+architecture Behavioral of Interogare_bancnote is 
+signal x :arraybancnota;
+begin  
+	x<=(1,5,10,50,100,200,500);
+	
+    process(clk,reset)
+	variable numarator1: number;
+	begin 	  
+		if (clk ='1')and (clk'event)  then
 
-            pozitie<=pozitie+1;
-            
-            bancnota_curenta<=numarator;
-            end if;
-            if(pozitie=7)then
-            pozitie<=0;
-            end if;
-            tip_bancnota<=bancnota_curenta;
-            codout<=pozitie;
-        end if;
-        
-    end process;
+			if numarator1 =6 or reset='1' then
+				numarator1:=0; 
+			else
+				numarator1:=numarator1+1;
+			end if;
+		end if;
+		pozitie_bancnota<=numarator1;
+		tip_bancnota<=x(numarator1);
+		
+	end process	 ;
 
 end Behavioral;
